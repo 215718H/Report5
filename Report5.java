7ca62324b5441a2f173dcd576264340572bac7eb
Ikki Matsuo <e215718@ie.u-ryukyu.ac.jp>
2021-12-19 18:24:32 +0900

    3rd commit

--- Report5.java
+++ Report5.java
@@ -1,13 +1,7 @@
 class Report5{
     public static void main(String[] args){
-        try{
-            String str = null;
-
-            System.out.println(str.length());
-
-        }catch(NullPointerException e){
-            System.out.println("NullPointerExceptionの例外発生");
-            System.out.println(e.getMessage()); //エラーメッセージを取得。
-        }
+            String str = "百二十三";
+            int value = Integer.parseInt(str);
+            System.out.println(value);
     }
 }
